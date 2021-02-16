2ed2af27da6a35cbea204d7550569572c7a083b5
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-16 15:34:02 -0500

    fixed syntax issues

--- src/particles/NavBar.js
+++ src/particles/NavBar.js
@@ -1,16 +1,17 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
 
 const NavBar=()=>{
     return(
-        <NavBar bg='dark' variant='dark'>
-            <NavBar.Brand href='/'>Les Livres</NavBar.Brand>
+        <Navbar bg='dark' variant='dark'>
+            <Navbar.Brand href='/'>Les Livres</Navbar.Brand>
             <Nav className='mr-auto'>
                 <Nav.Link href='/'>Home</Nav.Link>
                 <Nav.Link href='/'>Search</Nav.Link>
                 <Nav.Link href='/collection'>Collection</Nav.Link>
             </Nav>
-        </NavBar>
+        </Navbar>
     )
 }
 export default NavBar;
