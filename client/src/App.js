7c8dcab2e6cfd78ae53c44e90aca2ab274db461d
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-16 15:40:56 -0500

    moved file into client folder

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import NavBar from './particles/NavBar';
+import LandingPage from './pages/LandingPage';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <LandingPage/>
+    </div>
+  );
+}
+export default App;
---
+++ client/src/pages/LandingPage.js
@@ -0,0 +1,15 @@
+import React,{useState,useEffect} from 'react';
+// import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
+
+const LandingPage=()=>{
+    return(
+        <Container>
+            <h1>Trying this fluid thing out</h1>
+            <p>
+                Could hunt for and conquer a cougar? I would love to, in fact I think I see one that I'd love to make love to, make her come to (in every sense).              
+            </p>
+        </Container>
+    )
+}
+export default LandingPage;
