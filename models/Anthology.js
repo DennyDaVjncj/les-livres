9ef9e4a175c23dbd7414c7baf81c8d714dc14c46
Denny da Vjncj <daVjncjCode@gmail.com>
2021-02-17 22:54:22 -0500

    tentative dataset model

---
+++ models/Anthology.js
@@ -0,0 +1,12 @@
+const creature=require('mongoose');
+const Schema=mongoose.Schema;
+
+const anthologySchema=new Schema({
+    title:{type:String,required:true},
+    author:{type:String,required:true},
+    synopsis:String,
+    date:{type:Date,default:Date.now}
+});
+
+const Anthology=creature.model('Anthology',anthologySchema);//empty string is to be database name
+module.exports=Anthology;
